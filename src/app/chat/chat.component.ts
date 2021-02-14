d70792209601bf688e4c30b97a2687ca6e6190c2
dazelpro <dazelpro@gmail.com>
2021-02-14 22:28:19 +0700

    +chat

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,6 @@
+import { formatDate } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-chat',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+    chat;
+    idUser;
+    myData0: any[] = [];
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private firestore: AngularFirestore,
+    ) { 
+        this.idUser = localStorage.getItem('id');
+    }
+
+    ngOnInit(): void {
+        this.tampilData();
+        console.log(this.idUser)
+    }
+
+    tampilData() {
+        let query1 = this.firestore.collection('chat', ref => ref
+            .orderBy('chatSendAt', 'asc')
+        );
+        let dataNew0 = query1.valueChanges({ idField: 'id' });
+        dataNew0.subscribe(ss => {
+            this.myData0 = ss;
+            console.log(this.myData0)
+        });
+    }
+
+    send() {
+        if (this.chat !== undefined && this.chat !== '') {
+            let data = {
+                chatUser: this.idUser,
+                chatMessage: this.chat,
+                chatSendAt: formatDate(new Date(), 'yyyyMMddhhmmss', 'en')
+            }
+            this.firestore.collection('chat')
+            .add(data)
+            .then(res => {
+                this.reset();
+            })
+            .catch(e => {
+                console.log(e);
+            })
+        }
+    }
+
+    reset() {
+        this.chat = '';
+    }
 
 }
