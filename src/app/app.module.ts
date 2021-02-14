7bd26e667f3294bdd06f5ffe10f1ffa3ed51d5b4
dazelpro <dazelpro@gmail.com>
2021-02-14 20:58:51 +0700

    + login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from "@angular/fire/firestore";   //import baris ini
+import { environment } from 'src/environments/environment';
+import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './chat/chat.component';
+import { AppRoutingModule } from './app.routing.module';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        LoginComponent,
+        ChatComponent
     ],
     imports: [
         BrowserModule,
         FormsModule,
-        
+        AppRoutingModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),    //import baris ini
+        AngularFirestoreModule  //import baris ini
     ],
     providers: [],
     bootstrap: [AppComponent]
